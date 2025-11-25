8148fbc735fbbc897eacd525901e2851f97649eb
ARMAAN MISHRA <mishraarmaan999@gmail.com>
2025-11-25 21:49:19 +0530

    ass

--- Learn-c/7-String/String functions/4-strcmp.c
+++ Learn-c/7-String/String functions/4-strcmp.c
@@ -2,7 +2,11 @@
 #include <string.h>
 
 int main() {
-    int a = strcmp("far", "farm");
-    printf("The value of strcmp is %d", a);
+    int a = strcmp("far", "far"); // output is 0
+    int b = strcmp("far", "farmer"); // output is negative value
+    int c = strcmp("farmer", "far"); // output is positive value
+    printf("The value of strcmp is %d\n", b);
+    printf("The value of strcmp is %d\n", a);
+    printf("The value of strcmp is %d", c);
     return 0;
 }
