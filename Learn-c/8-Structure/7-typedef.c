d821d20f2235beb6bf730cd155ad402b65af7f5f
ARMAAN MISHRA <mishraarmaan999@gmail.com>
2025-11-27 02:10:21 +0530

    add

--- Learn-c/8-Structure/7-typedef.c
+++ Learn-c/8-Structure/7-typedef.c
@@ -10,16 +10,18 @@ typedef struct employee
     int code;
     float salary;
     char name[20];  
-}emp;
+}emp; 
+// typedef creates an alias emp for struct employee
 
 int main(){
     emp e1;
+    emp *ptr=&e1;
     e1.code=1001;
     e1.salary=34.56;
     strcpy(e1.name, "Armaan");
     
     printf("%d %f %s\n", e1.code, e1.salary, e1.name);
-    
+    printf("%d %f %s\n", ptr->code, ptr->salary, ptr->name);
     return 0;   
 
 }
