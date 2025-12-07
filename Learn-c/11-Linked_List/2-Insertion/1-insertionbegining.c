3d9e5f00b9f2ca494213ede63ad8fb8a681f0804
ARMAAN MISHRA <mishraarmaan999@gmail.com>
2025-12-07 19:24:43 +0530

    add

---
+++ Learn-c/11-Linked_List/2-Insertion/1-insertionbegining.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* insertatfirst(struct Node *head, int data)
+{
+    struct Node *ptr = (struct Node*)malloc(sizeof(struct Node));
+    ptr->next = head;
+    ptr->data = data;
+    return ptr;
+}
+
+int main() {
+    struct Node* head;
+    struct Node* second;
+    struct Node* third;
+
+    // Allocate memory
+    head = (struct Node*)malloc(sizeof(struct Node));
+    second = (struct Node*)malloc(sizeof(struct Node));
+    third = (struct Node*)malloc(sizeof(struct Node));
+
+    // Assign data
+    head->data = 1;
+    head->next = second;
+
+    second->data = 2;
+    second->next = third;
+
+    third->data = 3;
+    third->next = NULL;
+    
+    // Insert at beginning
+    head = insertatfirst(head, 10);
+
+
+    // Print linked list
+    struct Node* ptr = head;
+    while (ptr != NULL) {
+        printf("%d -> ", ptr->data);
+        printf("ptr address: %p\n", (void*)ptr);
+        ptr = ptr->next;
+    }
+    printf("NULL\n");
+
+    // Free memory
+    free(head);
+    free(second);
+    free(third);
+
+    return 0;
+}
